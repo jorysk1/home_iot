c5f58700c8b5ad40a75d0f5965f446229a3807d5
jorysk1 <joryskoja@gmail.com>
2024-10-28 11:04:19 +0100

    La LED s'allume

--- src/main.c
+++ src/main.c
@@ -5,10 +5,19 @@
  */
 
 #include <stdio.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/gpio.h>
+
+#define LED_YELLOW_NODE DT_ALIAS(led_yellow)
+
+const struct gpio_dt_spec led_yellow_gpio = GPIO_DT_SPEC_GET_OR(LED_YELLOW_NODE, gpios, {0});
+
+
 
 int main(void)
 {
 	printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-
+	gpio_pin_configure_dt(&led_yellow_gpio, GPIO_OUTPUT_HIGH);
 	return 0;
 }
+
